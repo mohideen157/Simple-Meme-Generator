f4eff90ca60fd6fa594aec66159bbd68b2dba575
mohideen157 <maabdulkhader157@gmail.com>
2022-02-09 00:30:33 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// eslint-disable-next-line no-unused-vars
+import Headers from './comp/Headers/Headers';
+
+
+
+export default function App() {
+  return (
+    
+      <Headers />
+   
+  );
+}
---
+++ src/comp/Headers/Headers.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Headers.css';
+import MemeGenerator from '../MemeGenerator/MemeGenerator';
+
+
+const Headers = () => {
+    return (
+        <>
+                <h1>Sample meme Generator</h1>
+            
+            <MemeGenerator />
+       
+                
+            </>
+         );
+};
+
+export default Headers;
